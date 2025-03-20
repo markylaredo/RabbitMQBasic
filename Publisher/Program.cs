3cd5db626acd5bc8bb33d4a26b9272b05638ee34
markylaredo <markanthonylaredo@gmail.com>
2025-03-20 12:09:31 +0800

    Initial commit

---
+++ Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "task_queue", durable: true, exclusive: false,
+    autoDelete: false, arguments: null);
+
+
+while (true)
+{
+    var now = DateTime.UtcNow.AddHours(8);
+    var message = $"{now:dddd MMMM yyyy HH:mm:ss.fff}";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    var properties = new BasicProperties
+    {
+        Persistent = true
+    };
+
+    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "task_queue", mandatory: true,
+        basicProperties: properties, body: body);
+
+    Console.WriteLine($" [x] Sent {message}");
+    await Task.Delay(1000);
+}
